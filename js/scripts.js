efbdfbcd203e427125ffc5b34d4183369bb8e4eb
ArturWitkowski <awitkowski@aerobit.pl>
2017-02-03 09:01:36 +0100

    Zadanie 8.6 poprawki do if

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,14 @@
 
 function getTriangleArea(a, h) {
 	
-	if (a <= 0 || h <= 0) return "Nieprawidłowe dane";
-	
-	else { 
-		
-		return a*h/2;
-		
+	if (a <= 0 || h <= 0) {
+		return "Nieprawidłowe dane";
+	} else {
+		return a * h / 2;
 	}
-	
 }
 
-console.log(getTriangleArea(10,6));
+console.log(getTriangleArea(10, 6));
 
 var triangle1Area = getTriangleArea(10, 15),
 	triangle2Area = getTriangleArea(-10, 15),
